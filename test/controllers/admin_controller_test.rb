7d9a92e4007d9a91069619d36a54ca0eb08edb32
Nailok <lexitor34@gmail.com>
2020-12-14 01:20:53 +0400

    Add users controller test

---
+++ test/controllers/admin_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionController::TestCase
+include AuthHelper
+  test "should get show" do
+    user = create(:user)
+    get :show, params: { id: user.id }
+    assert_response :success
+  end
+end
